a60de2ede8b3727c45e6b1e7d03ec708c7833b6e
MellyMel Melnyk <117057055+braja104@users.noreply.github.com>
2022-10-31 18:38:30 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('\N{u+1f963}  Omega 3 and Blueberry OatMeal')
+streamlit.text('\N{U+1F963}  Omega 3 and Blueberry OatMeal')
 streamlit.text('kale,Spinach and Rocket Smoothie')
 streamlit.text('Boiled_Egg Half Boil')
