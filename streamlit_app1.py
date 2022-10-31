e50a39f52bd1181670e41c45f0187c5bee439603
MellyMel Melnyk <117057055+braja104@users.noreply.github.com>
2022-10-31 17:43:30 +0530

    Update streamlit_app1.py
    Added new line

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry OatMeal')
+streamlit.text('kale,Spinach and Rocket Smoothie')
+streamlit.text('Boiled_Egg Half Boil')
