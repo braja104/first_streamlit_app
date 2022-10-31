c23cea17859a71ebe254d730540c52f1e60d86e9
MellyMel Melnyk <117057055+braja104@users.noreply.github.com>
2022-10-31 17:54:09 +0530

    Update streamlit_app1.py
    added smiley emoji

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text(':bowl_with_spoon: Omega 3 and Blueberry OatMeal')
+streamlit.text(''type:smiley:' Omega 3 and Blueberry OatMeal')
 streamlit.text('kale,Spinach and Rocket Smoothie')
 streamlit.text('Boiled_Egg Half Boil')
