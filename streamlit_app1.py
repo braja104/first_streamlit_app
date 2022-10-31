ae561b2d0a44a3ad9e05c57e2126591a040f25f1
MellyMel Melnyk <117057055+braja104@users.noreply.github.com>
2022-10-31 18:51:40 +0530

    Update streamlit_app1.py
    added fruit smoothie

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -3,7 +3,7 @@ streamlit.title('My Parents New Health Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('\N{pot of food}  Omega 3 and Blueberry OatMeal')
 streamlit.text('\N{cherries}kale,Spinach and Rocket Smoothie')
-streamlit.text('\N{egg}Boiled_Egg Half Boil
+streamlit.text('\N{egg}Boiled_Egg Half Boil)
 streamlit.header('Build Your Own Fruit Smoothie')
 streamlit.text('\N{grapes} Grape Juice')
 streamlit.text('\N{melon} Melon')
