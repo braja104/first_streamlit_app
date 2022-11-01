a0e116936a17a0a8fde430512ba418dd6772e193
MellyMel Melnyk <117057055+braja104@users.noreply.github.com>
2022-11-01 11:40:48 +0530

    Update streamlit_app1.py
    importind panda

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -9,3 +9,6 @@ streamlit.text('\N{grapes} Grape Juice')
 streamlit.text('\N{melon} Melon')
 streamlit.text('\N{peach} Peach')
 streamlit.text('\N{blueberries} Blueberries')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
